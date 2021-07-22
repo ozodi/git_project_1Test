1666bf4e13d7fa27cb3bf28eebcff5d742b6f17a
Ozodi Rumiev <rumiev0828@gmail.com>
2021-07-22 09:51:21 -0500

    initial commit

--- src/day01_GitHub/HelloWorld.java
+++ src/day01_GitHub/HelloWorld.java
@@ -1,11 +1,16 @@
 package day01_GitHub;
 
+import java.time.LocalDate;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
 
         System.out.println("Hello git World");
 
+       LocalDate DoF= LocalDate.of(2020,1,20);
+
+        System.out.println(DoF);
     }
 
 
